fa3bfd6f9f9da65c2e675e6fcd60213094eb0c1b
sunbrandon <sunabrandon@gmail.com>
2025-10-21 10:50:54 -0700

    feat: read filename

---
+++ path.py
@@ -0,0 +1,12 @@
+def main():
+    print("ComputeDronePath")
+    print()
+    
+    filename = input("Enter the name of file: ")
+    
+    if not filename:
+        print("No filename entered.")
+        return
+    
+if __name__ == "__main__":
+    main()
